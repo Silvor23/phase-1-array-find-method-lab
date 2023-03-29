7ae91a87c52faa4df86b157fb1b001e0d69eaeb4
abdullah <abdullah.karkarli@ogr.iu.edu.tr>
2023-03-29 23:02:27 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(array){
+  const target = array.find((element) => element.result === "W");
+  if (typeof target === 'undefined') {
+  return undefined;
+  }
+  else {return target.year};
+}
